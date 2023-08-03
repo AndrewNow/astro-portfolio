4b4208c6579800b3d67336320d66523881224fdd
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-08-03 19:16:27 -0400

    pushing

--- src/components/utils/linkHover.js
+++ src/components/utils/linkHover.js
@@ -1,14 +1,14 @@
 import { animate, stagger } from 'motion';
 
-// Function to create and animate characters on hover for all instances
+// Function to animate characters on hover for all instances
 const animateCharacters = () => {
   const animatedTextElements = document.querySelectorAll('#link-hover-text');
 
   animatedTextElements.forEach((link, i) => {
     const characters = link.textContent.trim().split('');
-    
     link.innerHTML = '';
 
+    
     characters.forEach((char) => {
       const span = document.createElement('span');
       span.innerText = char;
@@ -17,35 +17,30 @@ const animateCharacters = () => {
       link.appendChild(span);      
     });
     
-    
     const staggeredChars = link.querySelectorAll('.staggered-character');
-
 
     const handleHoverStart = () => {
       animate(staggeredChars, {
         transform: `translateY(-110%)`,
       }, {
-        delay: stagger(0.05, { from: "center" }),
-        duration: .35,
+        delay: stagger(0.05),
+        duration: .25,
         easing: [.6, 0.4, 0, 0.2]
       });
-      staggeredChars.forEach(char => {
-        // animate shadow upon hover
-        animate(char, {
-          textShadow: [
-            `0px 1em 1px var(--black)`,
-            `0px 1em 3px var(--grey-300)`,
-            `0px 1em 0px var(--grey-200)`,
-            `0px 1em 1px var(--grey-100)`,
-            `0px 1em 10px var(--yellow)`,
-          ],
-        }, {
-          delay: (Math.random() * .95) + 0.05,
-          easing: [.6, 0.4, 0, 0.2],
-          duration: 1.6,
-          direction: "alternate",
-          repeat: Infinity,
-        })
+      animate(staggeredChars, {
+        textShadow: [
+          null,
+          `0px 1em 0px var(--black)`,
+          `0px 1.1em 0px var(--black)`,
+          `0px 1em 0px var(--yellow)`,
+          `0px 1em 0px var(--black)`,
+          `0px 1em 0px var(--black)`,
+        ],
+      }, {
+        delay: stagger(0.03),
+        easing: [.6, 0.4, 0, 0.2],
+        duration: .8,
+        direction: "reverse",
       })
     }
 
@@ -53,8 +48,8 @@ const animateCharacters = () => {
       animate(staggeredChars, {
         transform: `translateY(0px)`,
       }, {
-        delay: stagger(0.05, { from: "center" }),
-        duration: .35,
+        delay: stagger(0.035, { from: "center" }),
+        duration: .3,
         easing: [0.2, 0, 0.4, 0.1]
       });
       staggeredChars.forEach(char => {
@@ -66,26 +61,14 @@ const animateCharacters = () => {
         }, {
           delay: .2,
           easing: [.6, 0.4, 0, 0.2],
-          duration: 0.5,
+          duration: 0.15,
         })
       })
     }
 
-    // Mouseenter event
-    link.addEventListener('mouseenter', handleHoverStart);
-    
-    // Mouseleave event
+    link.addEventListener('mouseenter', handleHoverStart);    
     link.addEventListener('mouseleave', handleHoverEnd);
   });
-};
-
-
-function mapRangeTo01(value, x) {
-  return (value - 1) / (x - 1);
-}
-// Function to get a random number between min and max (inclusive)
-const getRandomDelay = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // Call the function to animate all instances
